60d8e5962303f09339a2ac506bf16721333a08e9
Chloe bo Kim <chloe@perfitt.io>
2022-05-22 19:26:13 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-418cdd37d74f27cb08e1.js"
+    "url": "webpack-runtime-cbc7488ae22558476a31.js"
   },
   {
-    "url": "styles.6e4fdc2f3b1b83a07768.css"
+    "url": "styles.01e60c30b4d6086a39ae.css"
   },
   {
     "url": "styles-9605c3fbd8ad08ecf405.js"
@@ -38,18 +38,18 @@ self.__precacheManifest = [
     "url": "framework-727eca7a674e10b21a9f.js"
   },
   {
-    "url": "app-78f666c9e0fae77286a9.js"
+    "url": "app-a98d45703855d37acf5b.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-38fe08ef875e5952d5f0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0bb45d5d99abcb2a31ea5644ab67708d"
+    "revision": "b7b9775a7e2127ee36726346c8e92118"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "95d869f664477aa3f17292e135b182ac"
+    "revision": "d5744216a1a101dad6f737f28e1ca888"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
@@ -73,7 +73,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-78f666c9e0fae77286a9.js`))) {
+  if (!resources || !(await caches.match(`/app-a98d45703855d37acf5b.js`))) {
     return await fetch(event.request)
   }
 
