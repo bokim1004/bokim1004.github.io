9fcafded031165b3baeccb521277f5d246c429fc
chloe <chloe@perfitt.io>
2022-11-12 21:23:49 +0900

    블로그 좋아요 이모티콘 나오게

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,7 +11,7 @@ import { SocialShare } from '../components/social-share'
 import { SponsorButton } from '../components/sponsor-button'
 import { Bio } from '../components/bio'
 import { PostNavigator } from '../components/post-navigator'
-// import { Disqus } from '../components/disqus'
+import { Disqus } from '../components/disqus'
 import { Utterances } from '../components/utterances'
 import * as ScrollManager from '../utils/scroll'
 
@@ -43,14 +43,14 @@ export default ({ data, pageContext, location }) => {
       <Elements.Hr />
       <Bio />
       <PostNavigator pageContext={pageContext} />
-      {/* {!!disqusShortName && (
+      {!!disqusShortName && (
         <Disqus
           post={post}
           shortName={disqusShortName}
           siteUrl={siteUrl}
           slug={pageContext.slug}
         />
-      )} */}
+      )}
       {!!utterances && <Utterances repo={utterances} />}
     </Layout>
   )
