e4654daf1ee0936de4b389aaebd069d46badc0e8
bokim1004 <chloe@perfitt.io>
2023-04-09 16:35:53 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-94b81729df52f668a60e.js"
+    "url": "webpack-runtime-6b1d9133b91bdaae31f8.js"
   },
   {
     "url": "styles.5895ac9eb6778c4fd0f8.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e6e03fbb79bf667cab4f954c5bd0a4f3"
+    "revision": "0698cfd8e95663344dcf9bf9c58df327"
   },
   {
     "url": "polyfill-a08aa06da4160a99b142.js"
