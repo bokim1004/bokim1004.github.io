22b0ac5985cb44162a2df8629cbb46be089c5442
chloe <akbk1004@gmail.com>
2023-06-03 20:21:02 +0900

    fix

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -3,22 +3,20 @@ import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
-import Image from "gatsby-image";
 
 export const ThumbnailItem = ({ node }) => {
-
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    return(
-        <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-             <div className={'thumbnailWrapper'}>
-                 {node.frontmatter.image && !isMobile && <img src={node.frontmatter.image} alt={"img"}/>}
-            <div key={node.fields.slug} className={'slug'}>
-                <h3>{node.frontmatter.title || node.fields.slug}</h3>
-                <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-             </div>
-        </Link>
-    )
+  // const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  return (
+    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
+      <div className={'thumbnailWrapper'}>
+        {node.frontmatter.image && (
+          <img src={node.frontmatter.image} alt={'img'} />
+        )}
+        <div key={node.fields.slug} className={'slug'}>
+          <h3>{node.frontmatter.title || node.fields.slug}</h3>
+          <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      </div>
+    </Link>
+  )
 }
-
-
